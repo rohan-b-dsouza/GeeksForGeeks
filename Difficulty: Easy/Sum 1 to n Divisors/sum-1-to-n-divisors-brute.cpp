b36c5fd5fbc7ff562bd15756810c1ddc40402256
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-12 13:08:17 +0530

    Update sum-1-to-n-divisors-brute.cpp

--- Difficulty: Easy/Sum 1 to n Divisors/sum-1-to-n-divisors-brute.cpp
+++ Difficulty: Easy/Sum 1 to n Divisors/sum-1-to-n-divisors-brute.cpp
@@ -1,1 +1,18 @@
+class Solution {
+  public:
+    int sum = 0;
+    int sumOfDivisors(int n) {
+        for (int i = 1; i <= n; i++) { // T.C => O(n)
+            for (int j = 1; j * j <= i; j++) { // T.C => O(n * sqrt(n)) 
+                if (i % j == 0) {
+                   sum += j;
+                   if (i / j != j) sum += i / j;
+                }
+            }
+        }
+        return sum;
+    }
+};
 
+// T.C => O(n * sqrt(n)) 
+// S.C => O(1)
