7b487ea03a0958c1c81693f806dbf66b8441deda
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-13 19:28:29 +0530

    Update count-digits.cpp

--- Difficulty: Easy/Count Digits/count-digits.cpp
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     // Function to count the number of digits in n that evenly divide n
@@ -12,11 +6,7 @@ class Solution {
         int cnt = 0;
         while (n != 0) {
             int digit = n % 10;
-            if (digit == 0) {
-                n = n / 10;
-                continue;
-            }
-            if (original % digit == 0) cnt++;
+            if ((digit != 0) && (original % digit == 0)) cnt++;
             n = n / 10;
         }
         return cnt;
@@ -24,19 +14,5 @@ class Solution {
     }
 };
 
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        Solution ob;
-        cout << ob.evenlyDivides(N) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
+// T.C => O(log(n)) --------{log to the base 10 as n is divided by 10 repeatedly}
+// S.C => O(1)
