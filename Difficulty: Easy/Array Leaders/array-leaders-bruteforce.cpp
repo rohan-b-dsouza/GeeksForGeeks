5bad28a46c6b18c7586453b3772a2fd2c38d2073
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-26 11:58:52 +0530

    Create array-leaders-bruteforce.cpp

---
+++ Difficulty: Easy/Array Leaders/array-leaders-bruteforce.cpp
@@ -0,0 +1,77 @@
+// Brute-Force Approach
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        int n = arr.size();
+        vector <int> leader;
+        for (int i = 0; i < n; i++) {
+            bool flag = false;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                leader.push_back(arr[i]);
+            }
+        }
+        return leader;
+    }
+};
+// T.C => O(n^2)
+// S.C => O(n)
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.leaders(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no leaders are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
