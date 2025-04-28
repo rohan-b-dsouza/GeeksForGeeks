b8116917745de1912ca64d9f09f8ad385c3dc374
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-04-28 19:05:19 +0530

    Create count-subarrays-with-given-xor-brute.java

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor-brute.java
@@ -0,0 +1,21 @@
+// Better - Solution =>
+
+class Solution {
+    public long subarrayXor(int arr[], int k) {
+        int n = arr.length;
+        long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int xor = 0;
+                for (int l = i; l <= j; l++) {
+                    xor ^= arr[l];
+                }
+                if (xor == k) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+
+// T.C => O(n^3)
+// S.C => O(1)
