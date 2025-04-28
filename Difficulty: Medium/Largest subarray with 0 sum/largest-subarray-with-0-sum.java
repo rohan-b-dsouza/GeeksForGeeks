e10b9481041deddb50ca68fe1c04df83ed313c1b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-04-28 15:27:21 +0530

    Update largest-subarray-with-0-sum.java

--- Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.java
@@ -1,30 +1,4 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine()); // Number of test cases
-        for (int g = 0; g < t; g++) {
-            // Read input array as a string, split by space, and convert to integers
-            String[] str = br.readLine().trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            // Print the result from maxLen function
-            System.out.println(new Solution().maxLen(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
+// Optimal Solution => (Prefix Sum Method)
 
 class Solution {
     int maxLen(int arr[]) {
@@ -48,3 +22,6 @@ class Solution {
         return maxlen;
     }
 }
+
+// T.C => O(n)
+// S.C => O(n) for the hashmap used
