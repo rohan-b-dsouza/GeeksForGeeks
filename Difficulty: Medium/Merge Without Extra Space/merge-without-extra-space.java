3b9f6fb263952b4b9c3132cd2876f854d82cf632
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-05-03 12:33:44 +0530

    Update merge-without-extra-space.java

--- Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
@@ -1,48 +1,4 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-
-        while (t-- > 0) {
-            String arr1[] = br.readLine().trim().split(" ");
-            String arr2[] = br.readLine().trim().split(" ");
-
-            int a[] = new int[arr1.length];
-            int b[] = new int[arr2.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(arr1[i]);
-            for (int i = 0; i < b.length; i++) b[i] = Integer.parseInt(arr2[i]);
-
-            Solution ob = new Solution();
-            ob.mergeArrays(a, b);
-
-            StringBuffer str = new StringBuffer();
-            for (int i = 0; i < a.length; i++) {
-                str.append(a[i] + " ");
-            }
-            System.out.println(str);
-            str = new StringBuffer();
-            for (int i = 0; i < b.length; i++) {
-                str.append(b[i] + " ");
-            }
-            System.out.println(str);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
-// User function Template for Java
+// Optimal - Solution (Shell sort inspired gap method) 
 
 class Solution {
     // Function to merge the arrays.
@@ -81,3 +37,6 @@ class Solution {
         
     }
 }
+
+// T.C => O(logn) * O(n) = O(n * log(n)) 
+// S.C => O(1)
