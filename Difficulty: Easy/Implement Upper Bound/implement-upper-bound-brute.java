90196a434441d6cbf3097f307e96fc6c8344f595
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-05-21 16:49:58 +0530

    Update and rename implement-upper-bound.java to implement-upper-bound-brute.java

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound-brute.java
@@ -0,0 +1,21 @@
+// Brute Force 
+
+
+class Solution {
+    int upperBound(int[] arr, int target) {
+        // code here
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > target) return i;
+        }
+        return arr.length;
+    }
+}
+
+
+
+
+
+
+
+// T.C => O(n)
+// S.C => O(1)
