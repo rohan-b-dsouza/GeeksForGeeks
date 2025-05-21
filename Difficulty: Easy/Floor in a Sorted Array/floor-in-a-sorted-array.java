379cba993f4adc299d87525bb31b2cc15908d62b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-05-21 19:47:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    static int findFloor(int[] arr, int x) {
+        // write code here
+        int floor = -1;
+        int low = 0;
+        int high = arr.length - 1;
+         while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                floor = mid;
+                low = mid + 1;
+            }
+            else   {
+                high = mid - 1;
+            }
+        }
+        return floor;
+    }
+}
