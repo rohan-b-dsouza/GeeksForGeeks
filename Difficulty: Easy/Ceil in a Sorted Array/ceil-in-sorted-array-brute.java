f3e00d052f6a6a6143bbe3164e1e96aa6301a2f1
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-06 14:29:29 +0530

    Update ceil-in-sorted-array-brute.java

--- Difficulty: Easy/Ceil in a Sorted Array/ceil-in-sorted-array-brute.java
+++ Difficulty: Easy/Ceil in a Sorted Array/ceil-in-sorted-array-brute.java
@@ -2,7 +2,6 @@
 
 class Solution {
     public int findCeil(int[] arr, int x) {
-        // code here
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= x) return i;
         }
