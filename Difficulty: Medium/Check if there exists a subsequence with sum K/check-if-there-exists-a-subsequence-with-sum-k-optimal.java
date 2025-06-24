829491fdeda728c9c279e32b46056f10be7a581b
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-24 19:53:20 +0530

    Update check-if-there-exists-a-subsequence-with-sum-k-optimal.java

--- Difficulty: Medium/Check if there exists a subsequence with sum K/check-if-there-exists-a-subsequence-with-sum-k-optimal.java
+++ Difficulty: Medium/Check if there exists a subsequence with sum K/check-if-there-exists-a-subsequence-with-sum-k-optimal.java
@@ -2,12 +2,14 @@
 
 class Solution {
     public static boolean checkSum(int index, int k, int[] arr, int n) {
-        if (k == 0) {
+        if (k == 0) {  // Base case: if the sum k is 0, a subsequence is found
             return true;
         }
-        if (k < 0 || index == n) {
+        if (k < 0 || index == n) {  // Base case: if k is negative or index has reached n, no valid subsequence can be found
             return false;
         }
+        // Recursive call: include the current element in the subsequence
+        // or exclude the current element from the subsequence
         return checkSum(index + 1, k - arr[index], arr, n) || checkSum(index + 1, k, arr, n);
     }
     public static boolean checkSubsequenceSum(int N, int[] arr, int K) {
