6fb4f861f04661fdafe239419fae0a06c5a2bc94
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-06-25 08:51:33 +0530

    Update count-subsequences-with-Sum-K-optimal-1.java

--- Difficulty: Medium/Perfect Sum Problem/count-subsequences-with-Sum-K-optimal-1.java
+++ Difficulty: Medium/Perfect Sum Problem/count-subsequences-with-Sum-K-optimal-1.java
@@ -2,7 +2,7 @@
 
 class Solution {
   public int getNumberOftargetSumSubsequences(int index, int target, int n, int[] nums) {
-    if (index == n) {
+    if (index == n) { // Base Case : Checks if valid subsequence is found (Also handles case where array has zeroes)
         return (target == 0) ? 1 : 0; 
     }
     // Recursive Case : Include element in subsequence or exclude it
@@ -18,3 +18,13 @@ class Solution {
 
 // T.C => O(2 ^ n)
 // S.C => O(n)
+
+// If array has no zeroes, then instead of the if condition used in above solution we can use -
+// if (k == 0) { // Base case: if sum is 0, one valid subsequence is found, count++;
+//             return 1;
+//         }
+// if (k < 0 || index == n) { // Base Case : No valid subsequence found
+//             return 0;
+// }
+// This code exits early in each recursive step whenever k < 0 or if valid subsequence is found
+
