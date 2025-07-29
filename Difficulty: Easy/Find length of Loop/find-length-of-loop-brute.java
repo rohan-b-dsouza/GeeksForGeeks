0c500846098c277c88a9d04d1a75826b49adbb8d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-29 10:38:15 +0530

    Update find-length-of-loop-brute.java

--- Difficulty: Easy/Find length of Loop/find-length-of-loop-brute.java
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop-brute.java
@@ -15,12 +15,15 @@ class Solution {
         if (head == null) return 0;
         Node temp = head;
         int cnt = 0;
+        // HashMap to store visited nodes and the cumulative length
         HashMap<Node, Integer> map = new HashMap<>();
         while (temp != null) {
             cnt++;
+            // If a node is visited already, then loop is detected hence return the running length - cumulative length of that current node
             if (map.containsKey(temp)) {
                 return cnt - map.get(temp);
             }
+            // Put the node and its cumulative length in the map
             map.put(temp, cnt);
             temp = temp.next;
         }
