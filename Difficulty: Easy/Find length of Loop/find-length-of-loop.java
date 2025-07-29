2d14f7471c897fb00d163b11a78e05e355dfe0a4
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-29 10:34:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.java
@@ -0,0 +1,27 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+*/
+
+class Solution {
+    // Function to find the length of a loop in the linked list.
+    public int countNodesinLoop(Node head) {
+        if (head == null) return 0;
+        Node temp = head;
+        int cnt = 0;
+        HashMap<Node, Integer> map = new HashMap<>();
+        while (temp != null) {
+            cnt++;
+            if (map.containsKey(temp)) {
+                return cnt - map.get(temp);
+            }
+            map.put(temp, cnt);
+            temp = temp.next;
+        }
+        return 0;
+    }
+}
