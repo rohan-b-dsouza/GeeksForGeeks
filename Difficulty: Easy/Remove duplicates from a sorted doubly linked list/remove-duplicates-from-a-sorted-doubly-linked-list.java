c56d3692569cb72726dbbe02db07cd2a8669b10e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-07-31 00:33:49 +0530

    Update remove-duplicates-from-a-sorted-doubly-linked-list.java

--- Difficulty: Easy/Remove duplicates from a sorted doubly linked list/remove-duplicates-from-a-sorted-doubly-linked-list.java
+++ Difficulty: Easy/Remove duplicates from a sorted doubly linked list/remove-duplicates-from-a-sorted-doubly-linked-list.java
@@ -1,19 +1,28 @@
-
+// Optimal
 
 class Solution {
   public Node removeDuplicates(Node head) {
     if (head == null || head.next == null) return head;
+    // Pointer to traverse the LL
     Node temp = head;
+    // Pointer to track the distinct node
     Node distinct = head;
+    // Traverse the LL
     while (temp != null) {
+      // If a distinct data node is found
       if (temp.data != distinct.data) {
+        // Link it to our distinct list and update the distinct pointer to this new distinct node
         distinct.next = temp;
         temp.prev = distinct;
         distinct = temp;
       }
       temp = temp.next;
     }
+    // In case all nodes are same, this statement ensures that only one node is returned
     distinct.next = null;
     return head;
   }
 }
+
+// T.C => O(n)
+// S.C => O(1)
