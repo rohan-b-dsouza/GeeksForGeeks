69f77db506ad33691e03b7ae7ef52fe84ba272bc
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-08-30 15:55:28 +0530

    Update shortest-job-first-optimal.java

--- Difficulty: Medium/Shortest Job first/shortest-job-first-optimal.java
+++ Difficulty: Medium/Shortest Job first/shortest-job-first-optimal.java
@@ -1,5 +1,5 @@
 // Optimal (This problem is exactly the implementation of SJF (Non - preemptive))
-
+// Note:- Arrival time for all processes is 0 as per problem
 // This is written exactly like the SJF non - preemptive algorithm
 class Solution {
     static int solve(int[] bt) {
@@ -9,7 +9,7 @@ class Solution {
         int totalWaitingTime = 0;
         for (int i = 0; i < n; i++) {
             sum += bt[i]; // exit time of current process
-            totalWaitingTime += (sum - bt[i]); // add waiting time of current process
+            totalWaitingTime += (sum - bt[i]); // add waiting time of current process (W.T = T.A.T - B.T) as A.T = 0 for all processes therefore, T.A.T = E.T -> hence, W.T = E.T - B.T
         }
         return Math.floorDiv(totalWaitingTime, n);
     }
@@ -22,10 +22,10 @@ class Solution {
         Arrays.sort(bt);
         int n = bt.length;
         int totalTime = 0;
-        int waitingTime = 0;
+        int waitingTime = 0; // waiting time for first process is 0
         for (int i = 0; i < n; i++) {
-            waitingTime += totalTime;
-            totalTime += bt[i];
+            waitingTime += totalTime; // add waiting time for current process
+            totalTime += bt[i]; // store cumulative waiting time
         }
         return Math.floorDiv(waitingTime, n);
     }
