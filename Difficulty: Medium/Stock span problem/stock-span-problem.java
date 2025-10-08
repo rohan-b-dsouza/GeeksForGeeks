d4bb7cf2f06787477703b099d44ccabcdb0f6287
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-10-08 21:19:49 +0530

    Update stock-span-problem.java

--- Difficulty: Medium/Stock span problem/stock-span-problem.java
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -1,3 +1,5 @@
+// Better (Refer Leetcode version of this problem 'Online Stock Span' which has a slight design twist)
+
 class Solution {
     public int[] preGreaterEle(int[] arr) {
        int n = arr.length;
@@ -21,20 +23,16 @@ class Solution {
     public ArrayList<Integer> calculateSpan(int[] arr) {
         int n = arr.length;
         ArrayList<Integer> ans = new ArrayList<>();
+        // Get PGE array of 'arr'
         int[] pge = preGreaterEle(arr);
+        // For each element, get its span using (i - index of its previous greater element)
         for (int i = 0; i < n; i++) {
             ans.add(i - pge[i]);
         }
+        // Return ans
         return ans;
-        
     }
 }
-
-
-
-
-
-
 
 // class Solution {
 //     public ArrayList<Integer> calculateSpan(int[] arr) {
@@ -53,4 +51,5 @@ class Solution {
 //     }
 // }
 
-
+// T.C => O(2 * n) + O(n) = O(3 * n) = O(n)
+// S.C => O(2 * n) + O(n) = O(3 * n) = O(n) (However O(n) in calculateSpan function is needed to store the 'ans' so its negligible)
