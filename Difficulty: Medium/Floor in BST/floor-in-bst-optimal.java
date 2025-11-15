edfd1a70cc74d31123e32e7e9a15a3f43bfa9c6c
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-15 20:08:16 +0530

    Update and rename floor-in-bst.java to floor-in-bst-optimal.java

--- Difficulty: Medium/Floor in BST/floor-in-bst-optimal.java
+++ Difficulty: Medium/Floor in BST/floor-in-bst-optimal.java
@@ -1,3 +1,5 @@
+// Optimal
+
 /*
 class Node {
     int data;
@@ -12,20 +14,28 @@ class Node {
 */
 
 class Solution {
-    
     public static int floor(Node root, int x) {
+        // Get the root node and initialize floor to -1
         Node node = root;
         int floor = -1;
+        // While node is not null
         while (node != null) {
+            // If node's data is smaller than equal to x
             if (node.data <= x) {
+                // Store it in floor
                 floor = node.data;
+                // Move right as we are looking for maximum value that is smaller than equal to x
                 node = node.right;
             }
+            // Else move left
             else {
                 node = node.left;
             }
         }
+        // Return floor
         return floor;
-        
     }
 }
+
+// T.C => O(h) --- {h = log (n) for a balanced BST and h = n for a skewed BST}
+// S.C => O(1)
