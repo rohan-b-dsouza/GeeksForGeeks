6a3e7260cab99f3c0eb913ce5b6f252821f7d556
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-11-30 21:09:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Convert Min Heap to Max Heap/convert-min-heap-to-max-heap.java
@@ -0,0 +1,43 @@
+// User function Template for Java
+
+class Solution {
+    static void heapifyDown(int[] arr, int ind, int n) {
+	  // Get the left and right child
+    int left = 2 * ind + 1;
+    int right = 2 * ind + 2;
+    // Set largest to ind initially
+    int largest = ind;
+    // If left is within bounds and left child is larger than largest
+    // update largest
+    if (left < n && arr[left] > arr[largest]) {
+      largest = left;
+    }
+    // If right is within bounds and right child is larger than largest
+    // update largest
+    if (right < n && arr[right] > arr[largest]) {
+      largest = right;
+    }
+    // Now if largest is equal to ind, it means the current node's 
+    // left or right child are not larger or not present, hence we don't swap
+    // Else, if largest is not equal to ind, it means we got a larger value
+    // either from left or right child, hence swap.
+    if (largest != ind) {
+        int temp = arr[ind];
+        arr[ind] = arr[largest];
+        arr[largest] = temp;
+        // After swapping call the heapifyDown function on largest to ensure further
+        // max-heap is maintained
+        heapifyDown(arr, largest, n);
+    }
+  }
+    static void buildMaxHeap(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapifyDown(arr, i, n);
+        }
+    }
+    static void convertMinToMaxHeap(int N, int arr[]) {
+        // code here
+        buildMaxHeap(arr);
+    }
+}
