8abc45c0b813f4fcbc082122a3ec8377c399ea51
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-12-24 16:15:44 +0530

    Update dfs-of-graph-optimal.java

--- Difficulty: Easy/DFS of Graph/dfs-of-graph-optimal.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph-optimal.java
@@ -1,17 +1,23 @@
-// Depth First Search (Refer notion notes)
+// Depth First Search
 
 class Solution {
     public void dfs(int V, ArrayList<ArrayList<Integer>> adj, int[] visited, ArrayList<Integer> ans, int currNode) {
+        // Add current node to the ans list
         ans.add(currNode);
+        // Recursively call dfs on all nodes connected to current node
         for (int node : adj.get(currNode)) {
+            // If node is not visited then call dfs on it
             if (visited[node] != 1) {
+                // Mark node as visited
                 visited[node] = 1;
+                // Call dfs on it
                 dfs(V, adj, visited, ans, node);
             }
         }}
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
         int V = adj.size();
         int[] visited = new int[V];
+        // Mark node 0 as visited
         visited[0] = 1;
         ArrayList<Integer> ans = new ArrayList<>();
         dfs(V, adj, visited, ans, 0);
