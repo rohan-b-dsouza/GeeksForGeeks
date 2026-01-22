0eb94bdb8aef3712cbd69bc4b056bae5d85a88ee
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-01-22 11:54:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void dfs(int sourceNode, ArrayList<ArrayList<Integer>> adj, Deque<Integer> stack, boolean[] visited) {
+        for (int node : adj.get(sourceNode)) {
+            if (!visited[node]) {
+                dfs(node, adj, stack, visited);
+            }
+        }
+        visited[sourceNode] = true;
+        stack.push(sourceNode);
+
+    }
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] visited = new boolean[V];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+            dfs(i, adj, stack, visited);
+           }
+        }
+        while(!stack.isEmpty()) {
+            ans.add(stack.poll());
+        }
+        return ans;
+    }
+}
