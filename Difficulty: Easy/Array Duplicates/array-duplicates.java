752cac180fafd60612a3454e8eeb55267041dbd8
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-02-02 16:48:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.java
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -1,14 +1,18 @@
+
+
 class Solution {
     public ArrayList<Integer> findDuplicates(int[] arr) {
-        // code here
         int n = arr.length;
         ArrayList<Integer> ans = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        for (int num : arr) {
-            if (set.contains(num)) {
-                ans.add(num);
+
+        for (int i = 0; i < n; i++) {
+            int idx = Math.abs(arr[i]) - 1;
+
+            if (arr[idx] < 0) {
+                ans.add(idx + 1);   // duplicate found
+            } else {
+                arr[idx] = -arr[idx]; // mark visited
             }
-            set.add(num);
         }
         return ans;
     }
