2991a60c2fac7f71d7f83f2ebf2c548ffe3d088e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-02-02 23:10:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.java
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.java
@@ -1,23 +1,26 @@
 class Solution {
     public static int getMaxArea(int arr[]) {
+        // code here
         int n = arr.length;
-        int maxArea = 0;
+        int ans = 0;
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            while(!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+            while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
                 int idx = stack.pop();
                 int nse = i;
                 int pse = stack.isEmpty() ? -1 : stack.peek();
-                maxArea = (int) Math.max((nse - pse - 1) * 1L * arr[idx], maxArea); 
+                int bars = (nse - pse)- 1;
+                ans = Math.max(ans, arr[idx] * bars);
             }
             stack.push(i);
         }
         while(!stack.isEmpty()) {
-            int idx = stack.pop();
-            int nse = n;
-            int pse = stack.isEmpty() ? -1 : stack.peek();
-            maxArea = (int) Math.max((nse - pse - 1) * 1L * arr[idx], maxArea);
+               int idx = stack.pop();
+                int nse = n;
+                int pse = stack.isEmpty() ? -1 : stack.peek();
+                int bars = (nse - pse) - 1;
+                ans = Math.max(ans, arr[idx] * bars);
         }
-        return maxArea;
+        return ans;
     }
 }
