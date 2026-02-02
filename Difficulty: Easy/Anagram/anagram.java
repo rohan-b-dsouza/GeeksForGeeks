8d3a23efcbd674b69d38addbd17f5545949ec8b7
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2026-02-02 14:16:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,15 +1,16 @@
 class Solution {
     public static boolean areAnagrams(String s1, String s2) {
         // code here
-        if (s1.length() != s2.length()) return false;
-        int[] str = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            str[s1.charAt(i) - 'a']++;
-            str[s2.charAt(i) - 'a']--;
+        int n = s1.length();
+        if (n != s2.length()) return false;
+        int[] hash = new int[26];
+        for (int i = 0; i < n; i++) {
+            hash[s1.charAt(i) - 'a']++;
+            hash[s2.charAt(i) - 'a']--;
         }
-        for (int num : str) {
-            if (num != 0) return false;
-        }
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] != 0) return false;
+        } 
         return true;
     }
 }
